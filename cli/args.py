5133d48cb44e5ec30ac4ac1c4d65cb76829b9b7d
atim45 <atikshsharma517@gmail.com>
2026-05-18 13:44:01 +0530

    Updated cli/args.py file

--- cli/args.py
+++ cli/args.py
@@ -1,4 +1,8 @@
 import argparse
 
 def get_args():
-    praser = argparse.ArgumentParser(description="ArguFormer V2 — AI Communication Intelligence Platform")
+    parser = argparse.ArgumentParser(description="ArguFormer V2 — AI Communication Intelligence Platform")
+    parser.add_argument("--input", type=str, required=True, help="Path to debate transcript .txt file")
+    parser.add_argument("--output", type=str, required=True, help="Path to output .json or a .csv file")
+    parser.add_argument("--model", type=str, default="both",choices=["lr", "transformer", "both", "all"], help="Which model(s) to run")
+    parser.add_argument("--lang, type=str,  ")
